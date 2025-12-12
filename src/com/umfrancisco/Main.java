a8eb02645396bcde68277a21f6c0ee8ca9c02f85
umfrancisco <francis.guitler@gmail.com>
2025-12-12 15:25:24 -0300

    added more data collecting methods

--- src/com/umfrancisco/Main.java
+++ src/com/umfrancisco/Main.java
@@ -1,8 +1,10 @@
 package com.umfrancisco;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -71,10 +73,18 @@ public class Main {
 				.count();
 		System.out.println("longTerm students? "+longTermCount);
 		
-		Arrays.stream(students)
+		var longTimeLearners = Arrays.stream(students)
 				.filter(s -> (s.getAge() - s.getAgeEnrolled() >= 7) && (s.getMonthsSinceActive() < 12))
 				.filter(s -> !s.hasProgrammingExperience())
 				.limit(5)
-				.forEach(System.out::println);
+				.toArray(Student[]::new);
+		
+		var learners = Arrays.stream(students)
+				.filter(s -> (s.getAge() - s.getAgeEnrolled() >= 7) && (s.getMonthsSinceActive() < 12))
+				.filter(s -> !s.hasProgrammingExperience())
+				.limit(5)
+				.collect(Collectors.toList());
+		
+		Collections.shuffle(learners);
 	}
 }
